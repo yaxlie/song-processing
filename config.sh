38e4e1e2e812b75c7b65ff2398a69957368f9307
yaxlie <m.lesnierowski@gmail.com>
2018-10-10 21:08:29 +0200

    Add config scripts for windows and linux

---
+++ config.sh
@@ -0,0 +1,6 @@
+VAMP_PATH ='~/vamp_plugins'
+export VAR
+mkdir '~/vamp_plugins'
+cp ./lib/pyin/pyin.so ~/vamp_plugins/pyin.so
+cp ./lib/pyin/pyin.cat ~/vamp_plugins/pyin.cat
+cp ./lib/pyin/pyin.n3 ~/vamp_plugins/pyin.n3
