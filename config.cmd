a50ef97bd71d1ef540d4bdeee5009edd26aebea4
yaxlie <m.lesnierowski@gmail.com>
2018-10-14 15:23:01 +0200

    Refactor libs

--- config.cmd
+++ config.cmd
@@ -1,5 +1,5 @@
 SetX VAMP_PATH "%appdata%/vamp_plugins" /m
 mkdir "%appdata%\vamp_plugins"
-COPY .\lib\pyin\pyin.dll %appdata%\vamp_plugins\pyin.dll
-COPY .\lib\pyin\pyin.cat %appdata%\vamp_plugins\pyin.cat
-COPY .\lib\pyin\pyin.n3 %appdata%\vamp_plugins\pyin.n3
+COPY .\lib\pyin-win\pyin.dll %appdata%\vamp_plugins\pyin.dll
+COPY .\lib\pyin-win\pyin.cat %appdata%\vamp_plugins\pyin.cat
+COPY .\lib\pyin-win\pyin.n3 %appdata%\vamp_plugins\pyin.n3
--- config.sh
+++ config.sh
@@ -1,6 +1,5 @@
-VAMP_PATH ='~/vamp_plugins'
-export VAR
-mkdir '~/vamp_plugins'
-cp ./lib/pyin/pyin.so ~/vamp_plugins/pyin.so
-cp ./lib/pyin/pyin.cat ~/vamp_plugins/pyin.cat
-cp ./lib/pyin/pyin.n3 ~/vamp_plugins/pyin.n3
+export VAMP_PATH=~/vamp_plugins
+mkdir ~/vamp_plugins
+cp ./lib/pyin-linux/pyin.so ~/vamp_plugins/pyin.so
+cp ./lib/pyin-linux/pyin.cat ~/vamp_plugins/pyin.cat
+cp ./lib/pyin-linux/pyin.n3 ~/vamp_plugins/pyin.n3
