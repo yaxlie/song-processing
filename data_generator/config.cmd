d7fcd0a14ec75feb2e5d87fbc8a197e1b56e6846
yaxlie <m.lesnierowski@gmail.com>
2018-12-14 20:07:29 +0100

    Configure new project structure

--- data_generator/config.cmd
+++ data_generator/config.cmd
@@ -3,3 +3,4 @@ mkdir "%appdata%\vamp_plugins"
 COPY .\lib\pyin-win\pyin.dll %appdata%\vamp_plugins\pyin.dll
 COPY .\lib\pyin-win\pyin.cat %appdata%\vamp_plugins\pyin.cat
 COPY .\lib\pyin-win\pyin.n3 %appdata%\vamp_plugins\pyin.n3
+PAUSE
