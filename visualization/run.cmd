17b7835ec202bca2bce1326fb5e7647dc1050957
yaxlie <m.lesnierowski@gmail.com>
2019-09-25 11:00:36 +0200

    Add save-midi feature

---
+++ visualization/run.cmd
@@ -0,0 +1,8 @@
+@echo off
+ECHO Uruchamianie serwera... (localhost:8000)
+
+ECHO Aby zatrzymaæ serwer, nale¿y u¿yæ kombinacji klawiszy ctrl + c
+
+dir /ad /b ".\songs" >".\songs\list"
+python -m http.server
+PAUSE
